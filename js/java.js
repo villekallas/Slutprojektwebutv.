c5b56d6130e69e0983b893cbc4e54870eecd2066
villekallas <ville.kallas@student.tabyenskilda.se>
2023-05-11 22:23:56 +0200

    V.2.2
    Slutpublicering
    Kommenterad och förbättrad kod

--- js/java.js
+++ js/java.js
@@ -3,7 +3,7 @@ const header= document.querySelector("header");
 window.addEventListener("scroll", function(){
 header.classList.toggle ("sticky", this.window.scrollY > 0);
 })
-
+/* koden ovan gör så att headern inte försvinner när man scrollar ner utan är konstant*/
 
 let menu = document.querySelector('#menu-icon');
 let navmenu = document.querySelector('.navmenu');
@@ -12,15 +12,9 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navmenu.classList.toggle('open')
 }
+/* koden ovan gör att när man trycker på menyknappen så får den öppnas menyn, genom att den får en ny class och då en ny style i style.css,
+vilket gör den synlig. */
 
 
-
-/*
-let search = document.querySelector('#searchbarid');
-
-search.onclick = () => {
-    search.classList.toggle('searchbaropen')
-}
-*/
 
 
